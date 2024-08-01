ddab143c1c7dc8f42b48b561516e348e5b6e4e8d
Kyohei-takiyama <takiyamakyohei@gmail.com>
2024-08-01 09:23:37 +0900

    test: parameter test 'each'

--- src/section2/sum.test.ts
+++ src/section2/sum.test.ts
@@ -1,6 +1,20 @@
 import { sum } from "./sum";
 
-it("1と2を足すと3になる", () => {
-  // tobeはmatcher関数
-  expect(sum(1, 2)).toBe(3);
+// it("1と2を足すと3になる", () => {
+//   // tobeはmatcher関数
+//   expect(sum(1, 2)).toBe(3);
+// });
+
+// it("1と-2を足すと3になる", () => {
+//   // tobeはmatcher関数
+//   expect(sum(1, -2)).toBe(-1);
+// });
+
+// パラメータ化テスト
+it.each`
+  a    | b     | expected
+  ${1} | ${2}  | ${3}
+  ${1} | ${-2} | ${-1}
+`("sum($a, $b)は$expectedを返す", ({ a, b, expected }) => {
+  expect(sum(a, b)).toBe(expected);
 });
